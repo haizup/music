8844e95e1e4559550c7f39bc6bd0b3fe43323878
zhanghaiz <zhanghaiz@hangyjx.com>
2019-01-24 17:41:58 +0800

    添加vue项目常用的文件及配置

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import fastclick from 'fastclick';
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl';
+
+// 去除点击事件300ms的延迟
+fastclick.attach(document.body);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +19,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
