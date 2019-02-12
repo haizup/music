27e471b7c300196b5f4abca21c636c05c817a82e
zhanghaiz <zhanghaiz@hangyjx.com>
2019-02-12 18:02:00 +0800

    添加页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,57 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
+
+const Recommend = (resolve) => {
+  import('components/recommend/recommend').then((module) => {
+    resolve(module);
+  });
+};
+
+const Singer = (resolve) => {
+  import('components/singer/singer').then((module) => {
+    resolve(module);
+  });
+};
+
+const Rank = (resolve) => {
+  import('components/rank/rank').then((module) => {
+    resolve(module);
+  });
+};
+
+const Search = (resolve) => {
+  import('components/search/search').then((module) => {
+    resolve(module);
+  });
+}
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: '/recommend'
+    },
+    {
+      name: '推荐页面',
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      name: '歌手页面',
+      path: '/singer',
+      component: Singer
+    },
+    {
+      name: '排行页面',
+      path: '/rank',
+      component: Rank
+    },
+    {
+      name: '搜索页面',
+      path: '/search',
+      component: Search
     }
   ]
 });
