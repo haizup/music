7c8ec9e4eeffe56599fccd454cdc80ab85f871ac
zhanghaiz <zhanghaiz@hangyjx.com>
2019-02-20 18:05:25 +0800

    添加推荐页面样式和获取歌单的功能

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,85 @@
 <template>
-  <div>推荐页面</div>
+  <div class="recommend" ref="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper"></div>
+
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌曲推荐</h1>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend';
+  import {ERR_OK} from 'api/config';
+
+  export default {
+    created() {
+      this._getRecommend();
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+
+          }
+        });
+      }
+    }
+  };
 </script>
 
 <style scoped lang="stylus">
+  @import "~common/stylus/variable";
+
+  .recommend
+    position: fixed;
+    top: 4.4rem;
+    bottom: 0;
+    width: 100%;
+    .recommend-content
+      height: 100%;
+      overflow: hidden;
+      .slider-wrapper
+        position: relative;
+        width: 100%;
+        overflow: hidden;
+      .recommend-list
+        .list-title
+          height: 3.25rem;
+          line-height: 3.25rem;
+          text-align: center;
+          font-size: $font-size-medium;
+          color: $color-theme;
+        .item
+          display: flex;
+          box-sizing: border-box;
+          align-items: center;
+          padding: 0 1rem 1rem 1rem;
+          .icon
+            flex: 0 0 3rem;
+            width: 3rem;
+            padding-right: 1rem;
+          .text
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            flex: 1;
+            line-height: 1rem;
+            font-size: $font-size-medium;
+            overflow: hidden;
+            .name
+              margin-bottom: .5rem;
+              color: $color-text;
+            .desc
+              color: $color-text-d;
+      .loading-container
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
 </style>
