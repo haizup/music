b37ffda164396be393bad43e0c651e16ef9e4d83
zhanghaiz <zhanghaiz@hangyjx.com>
2019-02-26 14:13:05 +0800

    图片懒加载设置

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import fastclick from 'fastclick';
+import VueLazyload from 'vue-lazyload';
 import components from 'base/index';
 
 import 'common/stylus/index.styl';
+
+// 使用懒加载
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+});
 
 // 去除点击事件300ms的延迟
 fastclick.attach(document.body);
